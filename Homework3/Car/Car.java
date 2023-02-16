2e2a9b780450170ec9d9750e33b3ca8831eb3580
BjornTr <seryygraf@gmail.com>
2023-02-16 14:42:53 +0200

    add Car functionality

---
+++ Homework3/Car/Car.java
@@ -0,0 +1,22 @@
+package Homework3.Car;
+
+public class Car {
+
+    public static void Start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private static void startElectricity() {
+
+    }
+
+    private static void startCommand() {
+
+    }
+
+    private static void startFuelSystem() {
+
+    }
+}
