fb33317ca1222f1685d8a9430d82e1e86fb3d58d
BjornTr <seryygraf@gmail.com>
2023-02-16 15:10:13 +0200

    add the program launcher to Main

--- Homework3/Main.java
+++ Homework3/Main.java
@@ -3,6 +3,7 @@ package Homework3;
 import static Homework3.Employee.Inputs.doInputs;
 import static Homework3.Employee.Inputs.getOutput;
 
+
 public class Main {
 
     public static void main(String[] args) {
