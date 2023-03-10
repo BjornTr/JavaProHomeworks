51b9a08ff9edfb2dfeea029b0e08c9ba4b74c6b0
BjornTr <seryygraf@gmail.com>
2023-03-10 11:59:29 +0200

    add checkSumSign method

--- Homework6/HomeWorkApp.java
+++ Homework6/HomeWorkApp.java
@@ -7,7 +7,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
         printThreeWords();
-
+        checkSumSign();
     }
 
     public static void printThreeWords() {
@@ -16,5 +16,19 @@ public class HomeWorkApp {
         System.out.println("Apple");
     }
 
+    public static void checkSumSign() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value for a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter value for b: ");
+        int b = scanner.nextInt();
+        scanner.close();
 
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Sum is positive");
+        } else {
+            System.out.println("Sum is negative");
+        }
+    }
 }
