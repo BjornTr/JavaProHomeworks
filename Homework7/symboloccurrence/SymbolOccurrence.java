038c56952acd9f25e1f6b7d988237d278e770131
BjornTr <seryygraf@gmail.com>
2023-03-18 21:07:39 +0200

    add findSymbolOccurrence function to search for a specific character in a given text

---
+++ Homework7/symboloccurrence/SymbolOccurrence.java
@@ -0,0 +1,21 @@
+package Homework7.symboloccurrence;
+
+import java.util.Scanner;
+
+public class SymbolOccurrence {
+
+    public static void findSymbolOccurrence() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String s = scanner.nextLine();
+        System.out.print("Enter a character: ");
+        char symbol = scanner.nextLine().charAt(0);
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == symbol) {
+                count++;
+            }
+        }
+        System.out.println("The character '" + symbol + "' appears " + count + " times in the string.");
+    }
+}
