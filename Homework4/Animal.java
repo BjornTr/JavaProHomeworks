f941ab035e209645a5a88eb74e6a1e8e30dca9fe
BjornTr <seryygraf@gmail.com>
2023-03-23 03:15:13 +0200

    updated Animal class to separate it from the AnimalWithBreed class and deleted type and breed in it

--- Homework4/Animal.java
+++ Homework4/Animal.java
@@ -3,8 +3,6 @@ package Homework4;
 public class Animal {
 
     private static int count;
-    protected String type;
-    protected String breed;
     protected String name;
     protected double run;
     protected double swim;
@@ -19,24 +17,23 @@ public class Animal {
 
     protected void run(double distance) {
         if (run >= distance) {
-            System.out.println(this.type + " named " + this.name + " - successfully ran - " + distance + "m.");
+            System.out.println(this.getClass().getSimpleName() + " named " + this.name + " - successfully ran - " + distance + "m.");
         } else {
-            System.out.println(this.type + " named " + this.name + " - WASTED");
+            System.out.println(this.getClass().getSimpleName() + " named " + this.name + " - WASTED");
         }
     }
 
     protected void swim(double distance) {
         if (swim >= distance) {
-            System.out.println(this.type + " named " + this.name + " - successfully swam - " + distance + "m.");
+            System.out.println(this.getClass().getSimpleName() + " named " + this.name + " - successfully swam - " + distance + "m.");
         } else {
-            System.out.println(this.type + " named " + this.name + " - WASTED");
+            System.out.println(this.getClass().getSimpleName() + " named " + this.name + " - WASTED");
         }
     }
 
     @Override
     public String toString() {
-        return "Animal: " + type +
-                ", Breed: " + breed +
+        return "Animal: " + this.getClass().getSimpleName() +
                 ", Name: " + name +
                 ", Can run " + run + "m. " +
                 ", Can swim " + swim + "m. \n";
