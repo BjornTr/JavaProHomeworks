60655593d06873da466d2854e1d7af29a6931f12
BjornTr <seryygraf@gmail.com>
2023-03-23 03:05:38 +0200

    add Class Horse with a constructor and own swim method

---
+++ Homework4/Horse.java
@@ -0,0 +1,22 @@
+package Homework4;
+
+public class Horse extends Animal {
+
+    private static int count;
+
+    public Horse(String name, double maxRunDistance, double maxSwimDistance) {
+        this.name = name;
+        this.run = maxRunDistance;
+        this.swim = maxSwimDistance;
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    @Override
+    protected void swim(double distance) {
+        System.out.println("Horses can't swim that far.");
+    }
+}
