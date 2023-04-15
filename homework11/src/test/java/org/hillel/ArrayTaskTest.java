f39d2e95f99a962f86972a0aaca8217101937246
BjornTr <seryygraf@gmail.com>
2023-04-16 00:24:10 +0300

    add tests for the methods in ArrayTask

---
+++ homework11/src/test/java/org/hillel/ArrayTaskTest.java
@@ -0,0 +1,69 @@
+package org.hillel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayTaskTest {
+
+    @Test
+    void testLastFour1() {
+        int[] array1 = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        int[] arrExp1 = {1, 7};
+        assertArrayEquals(arrExp1, ArraysTask.newArrayAfterLastFour(array1));
+    }
+
+    @Test
+    void testLastFour2() {
+        int[] array2 = {4, 4, 4, 4};
+        int[] arrExp2 = {};
+        assertArrayEquals(arrExp2, ArraysTask.newArrayAfterLastFour(array2));
+    }
+
+    @Test
+    void testLastFour3() {
+
+        int[] array3 = {1, 2, 3};
+        try {
+            ArraysTask.newArrayAfterLastFour(array3);
+            fail("RuntimeException expected if array doesn't contain 4");
+        } catch (RuntimeException e) {
+            System.out.println("Doesn't contain 4");
+        }
+    }
+
+    @Test
+    void testLastFour4() {
+
+        int[] array4 = {1, 2, 4, 3, 4, 5};
+        int[] arrExp4 = {5};
+        assertArrayEquals(arrExp4, ArraysTask.newArrayAfterLastFour(array4));
+    }
+
+    @Test
+    void testContainsOnesAndFours1() {
+        int[] array1 = {1, 1, 1, 4, 4, 1, 4, 4};
+        assertTrue(ArraysTask.containsOnesAndFours(array1));
+    }
+
+    @Test
+    void testContainsOnesAndFours2() {
+
+        int[] array2 = {1, 1, 1, 1, 1, 1};
+        assertFalse(ArraysTask.containsOnesAndFours(array2));
+    }
+
+    @Test
+    void testContainsOnesAndFours3() {
+
+        int[] array3 = {4, 4, 4, 4};
+        assertFalse(ArraysTask.containsOnesAndFours(array3));
+    }
+
+    @Test
+    void testContainsOnesAndFours4() {
+
+        int[] array4 = {1, 4, 4, 1, 1, 4, 3};
+        assertFalse(ArraysTask.containsOnesAndFours(array4));
+    }
+}
