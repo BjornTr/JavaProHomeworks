dae1f4186882643dc7ed858f6a2bdabdeb3601b6
BjornTr <seryygraf@gmail.com>
2023-04-28 16:45:36 +0300

    update methods calcOccurrence and findOccurrence ещ make them more accurate and c hanged tests for them

--- homework12/src/main/java/MethodsWithList/ListMethods.java
+++ homework12/src/main/java/MethodsWithList/ListMethods.java
@@ -1,5 +1,7 @@
 package MethodsWithList;
 
+import lombok.AllArgsConstructor;
+
 import java.util.*;
 
 public class ListMethods {
@@ -32,33 +34,26 @@ public class ListMethods {
         return uniqueList;
     }
 
-    public static void calcOccurrence(List<String> words) {
-
-        Collections.sort(words);
-        String currentWord = words.get(0);
-        int count = 1;
-
-        for (int i = 1; i < words.size(); i++) {
-            String word = words.get(i);
-            if (word.equals(currentWord)) {
+    public static int calcOccurrence(List<String> words, String word) {
+        int count = 0;
+        for (String w : words) {
+            if (w.equals(word)) {
                 count++;
-            } else {
-                System.out.println(currentWord + ": " + count);
-                count = 1;
-                currentWord = word;
             }
         }
-        System.out.println(currentWord + ": " + count);
-
+        return count;
     }
 
-    public static List<Map.Entry<String, Integer>> findOccurrence(List<String> words) {
+    public static List<Occurrence> findOccurrence(List<String> words) {
         Map<String, Integer> occurrenceMap = new HashMap<>();
         for (String word : words) {
             occurrenceMap.put(word, occurrenceMap.getOrDefault(word, 0) + 1);
         }
-        List<Map.Entry<String, Integer>> occurrences = new ArrayList<>(occurrenceMap.entrySet());
-        occurrences.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        List<Occurrence> occurrences = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : occurrenceMap.entrySet()) {
+            occurrences.add(new Occurrence(entry.getKey(), entry.getValue()));
+        }
+        occurrences.sort(Comparator.comparing(Occurrence::getCount).reversed());
         return occurrences;
     }
 }
