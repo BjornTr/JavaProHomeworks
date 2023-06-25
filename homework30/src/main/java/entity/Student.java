59e337fb2a2b66a60238198646a2f807479e5bbd
BjornTr <seryygraf@gmail.com>
2023-06-25 22:29:44 +0300

    add students entity with id, name, email fields and auto-incrementing id

---
+++ homework30/src/main/java/entity/Student.java
@@ -0,0 +1,20 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+}
