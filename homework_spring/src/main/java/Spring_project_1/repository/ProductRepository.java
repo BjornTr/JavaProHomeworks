af976319f8dd7d055b9fac2a9b311b2bcda07279
BjornTr <seryygraf@gmail.com>
2023-06-30 03:21:48 +0300

    upd List<Product> products to final

--- homework_spring/src/main/java/Spring_project_1/repository/ProductRepository.java
+++ homework_spring/src/main/java/Spring_project_1/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public class ProductRepository {
 
-    private List<Product> products;
+    private final List<Product> products;
 
 
     public ProductRepository() {
