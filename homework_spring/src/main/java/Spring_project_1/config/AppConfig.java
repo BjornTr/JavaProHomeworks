054b6f84083db692e15c8cfdd3552e2e7fc49800
BjornTr <seryygraf@gmail.com>
2023-06-30 03:29:10 +0300

    add a config for the future

---
+++ homework_spring/src/main/java/Spring_project_1/config/AppConfig.java
@@ -0,0 +1,11 @@
+package Spring_project_1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"Spring_project_1"})
+public class AppConfig {
+
+
+}
