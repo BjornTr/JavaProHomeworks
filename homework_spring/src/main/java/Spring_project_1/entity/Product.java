e131658f0be3221af6764340a672a80502fbfa33
BjornTr <seryygraf@gmail.com>
2023-06-30 03:19:34 +0300

    add entity Product with constructors and DTO for it

---
+++ homework_spring/src/main/java/Spring_project_1/entity/Product.java
@@ -0,0 +1,14 @@
+package Spring_project_1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+}
