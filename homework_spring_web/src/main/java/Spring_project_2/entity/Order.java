6d171feca75656da1021d47108d55fdeba8b1d03
BjornTr <seryygraf@gmail.com>
2023-07-04 03:58:05 +0300

    add entities Order and Product

---
+++ homework_spring_web/src/main/java/Spring_project_2/entity/Order.java
@@ -0,0 +1,20 @@
+package Spring_project_2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private UUID id;
+    private String date;
+    private double cost;
+    private List<Product> products;
+}
---
+++ homework_spring_web/src/main/java/Spring_project_2/entity/Product.java
@@ -0,0 +1,16 @@
+package Spring_project_2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private double cost;
+}
